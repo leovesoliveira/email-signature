70cb093d8685c1a8aa5ae671940f74942fa6bd71
Leonardo Alves de Oliveira <leoalves@uolinc.com>
2022-09-26 19:27:21 -0300

    make app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,189 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Button,
+  Center,
+  Input,
+  MantineProvider,
+  Title,
+} from "@mantine/core";
+import { IconCheck, IconCopy } from "@tabler/icons";
+import { createRef, useState } from "react";
+import EmailSignature from "./EmailSignature";
 
 function App() {
+  const [name, setName] = useState("Sherlock Holmes");
+  const [job, setJob] = useState("Consulting Detective");
+  const [line1, setLine1] = useState(
+    "221b Baker St, London NW1 6XE, United Kingdom"
+  );
+  const [line2, setLine2] = useState("Phone: +44 20 7224 3688");
+
+  const url_string = window.location.href;
+  const url = new URL(url_string);
+  const { bannerLink, bannerImg, logoLink, logoImg, linkedinLink } =
+    url.searchParams.get("data")
+      ? JSON.parse(atob(url.searchParams.get("data")))
+      : {};
+
+  const emailSignatureRef = createRef();
+
+  const [copied, setCopied] = useState(false);
+
+  const copy = () => {
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNodeContents(emailSignatureRef.current);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand("copy");
+    setCopied(true);
+    selection.removeAllRanges();
+    setTimeout(() => setCopied(false), 1000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <MantineProvider>
+      <Box
+        sx={(theme) => ({
+          backgroundColor: theme.colors.indigo[0],
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        })}
+      >
+        <Title
+          sx={(theme) => ({
+            paddingBottom: theme.spacing.md,
+          })}
+          order={1}
+          color="teal.9"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Email Signature
+        </Title>
+
+        <Box
+          sx={(theme) => ({
+            width: "658px",
+            display: "flex",
+            flexWrap: "wrap",
+          })}
+        >
+          <Input.Wrapper
+            sx={(theme) => ({
+              width: "22rem",
+              paddingBottom: theme.spacing.sm,
+              paddingRight: theme.spacing.sm,
+            })}
+            id="name"
+            label="Full Name"
+          >
+            <Input
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Input.Wrapper>
+
+          <Input.Wrapper
+            sx={(theme) => ({
+              width: "18.3rem",
+              paddingBottom: theme.spacing.sm,
+            })}
+            id="job-description"
+            label="Job Description"
+          >
+            <Input
+              id="job-description"
+              value={job}
+              onChange={(e) => setJob(e.target.value)}
+            />
+          </Input.Wrapper>
+
+          <Input.Wrapper
+            sx={(theme) => ({
+              width: "100%",
+              paddingBottom: theme.spacing.sm,
+            })}
+            id="line-1"
+            label="Line 1"
+          >
+            <Input
+              id="line-1"
+              value={line1}
+              onChange={(e) => setLine1(e.target.value)}
+            />
+          </Input.Wrapper>
+
+          <Input.Wrapper
+            sx={(theme) => ({
+              width: "100%",
+              paddingBottom: theme.spacing.xl,
+            })}
+            id="line-2"
+            label="Line 2"
+          >
+            <Input
+              id="line-2"
+              value={line2}
+              onChange={(e) => setLine2(e.target.value)}
+            />
+          </Input.Wrapper>
+        </Box>
+
+        <Center
+          sx={(theme) => ({
+            backgroundColor: "white",
+            padding: theme.spacing.xl,
+            borderRadius: theme.radius.md,
+            boxShadow: theme.shadows.md,
+            marginBottom: theme.spacing.xl,
+          })}
+        >
+          <div ref={emailSignatureRef}>
+            <EmailSignature
+              name={name}
+              job={job}
+              line1={line1}
+              line2={line2}
+              bannerLink={bannerLink}
+              bannerImg={bannerImg}
+              logoLink={logoLink}
+              logoImg={logoImg}
+              linkedinLink={linkedinLink}
+            />
+          </div>
+        </Center>
+
+        <Button color={copied ? "teal" : "indigo"} onClick={copy}>
+          {copied ? (
+            <span
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <IconCheck size={16} />
+              <span style={{ marginLeft: "0.5rem" }}>Copied</span>
+            </span>
+          ) : (
+            <span
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <IconCopy size={16} />
+              <span style={{ marginLeft: "0.5rem" }}>Copy Email Signature</span>
+            </span>
+          )}
+        </Button>
+      </Box>
+    </MantineProvider>
   );
 }
 
---
+++ src/Generate.js
@@ -0,0 +1,169 @@
+import {
+  Box,
+  Button,
+  CopyButton,
+  Input,
+  MantineProvider,
+  Title,
+} from "@mantine/core";
+import { IconCheck, IconCopy } from "@tabler/icons";
+import { useEffect, useState } from "react";
+
+function Generate() {
+  const [logoLink, setLogoLink] = useState("");
+  const [logoImg, setLogoImg] = useState("");
+  const [bannerLink, setBannerLink] = useState("");
+  const [bannerImg, setBannerImg] = useState("");
+  const [linkedinLink, setLinkedinLink] = useState("");
+
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    const data = { logoLink, logoImg, bannerLink, bannerImg, linkedinLink };
+    const dataStringBase64 = btoa(JSON.stringify(data));
+    setUrl(`${window.location.origin}?data=${dataStringBase64}`);
+  }, [logoLink, logoImg, bannerLink, bannerImg, linkedinLink]);
+
+  return (
+    <MantineProvider>
+      <Box
+        sx={(theme) => ({
+          backgroundColor: theme.colors.indigo[0],
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        })}
+      >
+        <Title
+          sx={(theme) => ({
+            paddingBottom: theme.spacing.md,
+          })}
+          order={1}
+          color="teal.9"
+        >
+          Generate Email Signature
+        </Title>
+
+        <Box
+          sx={(theme) => ({
+            width: "658px",
+            display: "flex",
+            flexWrap: "wrap",
+          })}
+        >
+          <Input.Wrapper
+            sx={(theme) => ({
+              width: "100%",
+              paddingBottom: theme.spacing.sm,
+            })}
+            id="logoLink"
+            label="Logo Link"
+          >
+            <Input
+              id="logoLink"
+              value={logoLink}
+              onChange={(e) => setLogoLink(e.target.value)}
+            />
+          </Input.Wrapper>
+
+          <Input.Wrapper
+            sx={(theme) => ({
+              width: "100%",
+              paddingBottom: theme.spacing.sm,
+            })}
+            id="logoImg"
+            label="Logo Image"
+          >
+            <Input
+              id="logoImg"
+              value={logoImg}
+              onChange={(e) => setLogoImg(e.target.value)}
+            />
+          </Input.Wrapper>
+
+          <Input.Wrapper
+            sx={(theme) => ({
+              width: "100%",
+              paddingBottom: theme.spacing.sm,
+            })}
+            id="bannerLink"
+            label="Banner Link"
+          >
+            <Input
+              id="bannerLink"
+              value={bannerLink}
+              onChange={(e) => setBannerLink(e.target.value)}
+            />
+          </Input.Wrapper>
+
+          <Input.Wrapper
+            sx={(theme) => ({
+              width: "100%",
+              paddingBottom: theme.spacing.sm,
+            })}
+            id="bannerImg"
+            label="Banner Image"
+          >
+            <Input
+              id="bannerImg"
+              value={bannerImg}
+              onChange={(e) => setBannerImg(e.target.value)}
+            />
+          </Input.Wrapper>
+
+          <Input.Wrapper
+            sx={(theme) => ({
+              width: "100%",
+              paddingBottom: theme.spacing.xl,
+            })}
+            id="linkedinLink"
+            label="LinkedIn Link"
+          >
+            <Input
+              id="linkedinLink"
+              value={linkedinLink}
+              onChange={(e) => setLinkedinLink(e.target.value)}
+            />
+          </Input.Wrapper>
+        </Box>
+
+        <CopyButton value={url}>
+          {({ copied, copy }) => (
+            <Button color={copied ? "teal" : "indigo"} onClick={copy}>
+              {copied ? (
+                <span
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <IconCheck size={16} />
+                  <span style={{ marginLeft: "0.5rem" }}>Copied</span>
+                </span>
+              ) : (
+                <span
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <IconCopy size={16} />
+                  <span style={{ marginLeft: "0.5rem" }}>
+                    Copy Link to Email Signature
+                  </span>
+                </span>
+              )}
+            </Button>
+          )}
+        </CopyButton>
+      </Box>
+    </MantineProvider>
+  );
+}
+
+export default Generate;
